8e11c838a42e956ee3614c732068366d82f33ae2
Ionut Benyi <benyi.ionut@yahoo.com>
2023-01-31 22:51:24 +0200

    frontend with cart

---
+++ client/src/app/services/store.service.ts
@@ -0,0 +1,55 @@
+﻿import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Order, OrderItem } from "../Shared/Order";
+import { Product } from "../Shared/Product";
+
+@Injectable()
+export class Store {
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    public products: Product[] = [];
+
+    public order: Order = new Order();
+
+    loadProducts(): Observable<void> {
+        return this.http.get<[]>("/api/products")
+            .pipe(map(data => {
+                this.products = data;
+                return;
+            }));
+    }
+
+    addToOrder(product: Product) {
+
+        
+        let item: OrderItem;
+
+        item = this.order.items.find(o => o.productId === product.id) as OrderItem;
+        
+        if (item) {
+            item.quantity++;
+            console.log("found it");
+        } else {
+            item = new OrderItem();
+            item.productId = product.id;
+            item.productTitle = product.title;
+            item.productArtId = product.artId;
+            item.productArtist = product.artist;
+            item.productCategory = product.category;
+            item.productSize = product.size;
+            item.unitPrice = product.price;
+            item.quantity = 1;
+
+            this.order.items.push(item);
+            console.log("adding it");
+        }
+
+    }
+
+
+}
